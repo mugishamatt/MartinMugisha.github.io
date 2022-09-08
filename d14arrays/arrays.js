84647537c9284ffbe75d0ae39dde2b20c312015b
mugishamatt <67480576+mugishamatt@users.noreply.github.com>
2022-09-08 15:50:53 -0500

    commit

---
+++ d14arrays/arrays.js
@@ -0,0 +1,43 @@
+"use strict";
+//You need the module.exports when testing in node.  Comment it out when you send your file to the browser
+module.exports = {maxOfThree: maxOfThree, sum: sum, multiply: multiply }; //add all of your function names here that you need for the node mocha tests
+
+/**
+ *
+ * @param {number} a is a number
+ * @param {number} b is a number
+ * @param {number} c is a number
+ * @returns {number} largest of a, b, c
+ */
+function maxOfThree(a, b, c) {
+  if (a > b) {
+    return a;
+  } else if (b > c) {
+    return b;
+  } else {
+    return c;
+  }
+}
+console.log(maxOfThree(10, 30, 20));
+/**
+ *
+ * @param {Array} arr of numbers
+ * @returns {number} sum of arr numbers
+ */
+function sum(arr) {
+  let tot = 0;
+  for (const number of arr) {
+    tot += number;
+  }
+  return tot;
+}
+/**
+ *
+ * @param {Array} arr of numbers
+ * @returns {number} sum of arr numbers
+ */
+function multiply(arr) {
+  let tot = 1;
+
+  return tot;
+}
