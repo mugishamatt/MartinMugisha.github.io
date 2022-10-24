eb698abf2fae440fc9162d503308f81f4aa36bc8
mugishamatt <67480576+mugishamatt@users.noreply.github.com>
2022-10-23 23:51:19 -0500

    sorting updates

--- cs315/FinalProject/cards/card.js
+++ cs315/FinalProject/cards/card.js
@@ -2,7 +2,7 @@ const arrProducts= [
     {
         url: "#",
         title: "AE Super Soft Hooded Flannel Shacket",
-        image: "../images/four.webp",
+        image: "./images/four.webp",
         category: "top",
         description: " new+ good quality",
         price:"34.99",
@@ -12,7 +12,7 @@ const arrProducts= [
     {
         url: "#",
         title: "AEO Plastic Frame Aviator Sunglasses",
-        image: "../images/sunglass.webp",
+        image: "./images/sunglass.webp",
         category: "accessories",
         description: "Online only",
         price:"45.99",
@@ -23,7 +23,7 @@ const arrProducts= [
     {
         url: "#",
         title: "AE Super Soft Hooded Flannel Shirt",
-        image: "../images/hoody.webp",
+        image: "./images/hoody.webp",
         category: "top",
         description: "online only",
         price:"45.99",
@@ -33,7 +33,7 @@ const arrProducts= [
     {
         url: "#",
         title: "AE Camo Baggy Cargo Jogger",
-        image: "../images/bottom2.webp",
+        image: "./images/bottom2.webp",
         category: "bottom",
         description: " this a nice quality",
         price:"45.99",
@@ -43,7 +43,7 @@ const arrProducts= [
     {
         url: "#",
         title: "AEO Plaid Ultra Soft Boxer Short 3-Pack",
-        image: "../images/bottom.webp",
+        image: "./images/bottom.webp",
         category: "bottom",
         description: " online only",
         price:"45.99",
@@ -53,7 +53,7 @@ const arrProducts= [
     {
         url: "#",
         title: "AE Road Tripper Hat",
-        image: "../images/hat2.webp",
+        image: "./images/hat2.webp",
         category: "accessories",
         description: " verrythis a nice quality",
         price:"45.99",
@@ -63,7 +63,7 @@ const arrProducts= [
     {
         url: "#",
         title: "AE Super Soft Hooded Flannel Shacket",
-        image: "../images/bottom1.webp",
+        image: "./images/bottom1.webp",
         category: "bottom",
         description: " good+good Quality",
         price:"45.99",
@@ -74,7 +74,7 @@ const arrProducts= [
     {
         url: "#",
         title: "AE Camo Baggy Cargo Jogger",
-        image: "../images/socks.webp",
+        image: "./images/socks.webp",
         category: "accessories",
         description: " new+ good quality",
         price:"40.99",
@@ -84,7 +84,7 @@ const arrProducts= [
     {
         url: "#",
         title: "AE Super Soft Hooded Flannel Shacket",
-        image: "../images/six.webp",
+        image: "./images/six.webp",
         category: "top",
         description: " good+good Quality",
         price:"49.99",
@@ -97,7 +97,7 @@ const arrProducts= [
 ]
 
 //need this to append
-const courseContainer = document.querySelector(".course-container")
+const productContainer = document.querySelector(".product-container")
 
 const displayProducts = (urlValue, titleValue,categoryValue,imageValue,
     descriptionValue, priceValue,discountValue)=>{
@@ -147,7 +147,7 @@ const displayProducts = (urlValue, titleValue,categoryValue,imageValue,
   discount.innerHTML=discountValue;
 
  //append element
-  courseContainer.appendChild(card);
+  productContainer.appendChild(card);
   card.appendChild(a);
   a.appendChild(category);
   a.appendChild(img);
@@ -173,3 +173,59 @@ const generateData = ()=>{
 };
 
 generateData();
+
+
+//sorting functions
+
+//sorting high to low
+function sortingDescending(x,y){
+    return parseFloat(y.price) - parseFloat(x.price);
+}
+let descending = arrProducts.sort(sortingDescending);
+
+
+//console.log( descending);
+
+
+//row to high by price
+
+function sortingAscending(a,b){
+  return parseFloat(a.price) - parseFloat(b.price);
+}
+let ascending= arrProducts.sort(sortingAscending)
+console.log( ascending);
+
+
+
+//filter
+let  filterTops = arrProducts.filter((ele)=>{
+  return ele.description=="online only";
+})
+
+//console.log(filterTops);
+
+
+
+
+// sorting :low to high
+
+const sortLowToHigh=document.querySelectorAll('input[name="sort"]')
+
+  sortLowToHigh.onchange=function(){
+    console.log("here")
+   
+    for(const radioBtn of sortLowToHigh){
+
+        let rad= radioBtn.innerHTML
+        console.log(rad)
+        
+        if(radioBtn.checked){
+            console.log(radioBtn.checked)
+           // 
+            if(radioBtn.value=="lower-to high")
+             descending;
+            
+            break;
+        }
+    }
+  }
